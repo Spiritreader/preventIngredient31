e8e4cbd716908cda9ef0a8021eb5fbec85fd8fad
Sam <earthstamper@gmail.com>
2019-04-13 00:27:06 +0200

    release candidate 1.2

---
+++ frontend/js/init.js
@@ -0,0 +1,165 @@
+let rowCounter = 0;
+let rowElement;
+let yolo = false;
+let menuAll;
+let menuDatePickr;
+let globalSelectedDate = new Date();
+let supplementTranslation;
+let menuFirstDay;
+let menuLastDay;
+
+function init() {
+    let isWeekend = (globalSelectedDate.getDay() === 6) || (globalSelectedDate.getDay() === 0);
+    if (isWeekend) {
+        globalSelectedDate.setDate(globalSelectedDate.getDate() + (1 + 7 - globalSelectedDate.getDay()) % 7);
+    }
+    document.getElementById("calendar-dateboi").value = globalSelectedDate.getDate() + "." + (globalSelectedDate.getMonth() + 1) + "." + globalSelectedDate.getFullYear()
+    $.get("/api", function (response) {
+        menuAll = response;
+        menuFirstDay = new Date(menuAll[0].date);
+        menuLastDay = new Date(menuAll[menuAll.length - 1].date);
+        menuDatePickr = flatpickr("#calendar-dateboi", {
+            minDate: menuFirstDay,
+            maxDate: menuLastDay,
+            "disable": [
+                function (date) {
+                    // return true to disable
+                    return (date.getDay() === 6 || date.getDay() === 0);
+                }
+            ],
+            "locale": {
+                "firstDayOfWeek": 1 // start week on Monday
+            },
+            dateFormat: "d.m.Y",
+            onChange: dateChanger
+        });
+        $.ajax({
+            dataType: "json",
+            url: "./supplements.json"
+        }).done(function (data) {
+            if (!yolo) {
+                // Show everything ;)
+                let hider = document.getElementsByClassName("hider")[0];
+                hider.parentNode.removeChild(hider);
+
+                supplementTranslation = data;
+                processCheckboxes(data);
+                onChangeCheckbox();
+                yolo = true;
+                showMenu(response, new Date(globalSelectedDate.getFullYear(), globalSelectedDate.getMonth(), globalSelectedDate.getDate()));
+            }
+        })
+
+    });
+
+}
+
+function dateChanger(selectedDates) {
+    let selectedDate = selectedDates[0];
+    if (selectedDate) {
+        globalSelectedDate = selectedDate;
+        showMenu(menuAll, selectedDate);
+    }
+}
+
+/**
+ * Time in days between two dates
+ * @param {*} dt1 StartDate
+ * @param {*} dt2 EndDate
+ */
+function tdiff(dt1, dt2) {
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
+}
+
+function processCheckboxes(supplements) {
+    let allergenKeys = Object.keys(supplements.Allergene).sort();
+    let additivesKeys = Object.keys(supplements.Zusatzstoffe);
+    let tagKeys = Object.keys(supplements.Kategorien).sort();
+    let allergens = document.querySelector("#collapseAllergens > div");
+    let additives = document.querySelector("#collapseSupplements > div");
+    let tags = document.querySelector("#collapseTags > div");
+
+    rowElement = document.createElement('div');
+    rowElement.setAttribute('class', 'row');
+    allergenKeys.forEach((key) => {
+        inject(allergens, supplements.Allergene, key, 'sups');
+    });
+    allergens.appendChild(rowElement);
+
+    rowElement = document.createElement('div');
+    rowElement.setAttribute('class', 'row')
+    additivesKeys.forEach((key) => {
+        inject(additives, supplements.Zusatzstoffe, key, 'sups');
+    });
+    additives.appendChild(rowElement);
+
+
+    rowElement = document.createElement('div');
+    rowElement.setAttribute('class', 'row')
+    tagKeys.forEach((key) => {
+        injectTags(tags, supplements.Kategorien, key, 'tags');
+    });
+    tags.appendChild(rowElement);
+}
+
+function inject(body, lookup, key, classInfo) {
+    let inputDiv = document.createElement('div');
+    inputDiv.setAttribute('class', 'col-lg-3 col-md-3 form-check');
+
+    //define label
+    let label = document.createElement('label');
+    label.setAttribute('class', 'form-check-label ' + classInfo + '-label');
+    label.setAttribute('for', key);
+    label.textContent = lookup[key] + " (" + key + ")";
+
+    //define input
+    let input = document.createElement('input');
+    input.setAttribute('class', 'form-check-input ' + classInfo + '-checkbox');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('id', key);
+
+    //append checkbox and label to inputDiv
+    inputDiv.appendChild(input);
+    inputDiv.appendChild(label);
+
+    rowElement.appendChild(inputDiv);
+
+    if (rowElement.children.length == 4) {
+        body.appendChild(rowElement);
+        rowElement = document.createElement('div');
+        rowElement.setAttribute('class', 'row')
+    }
+}
+
+function injectTags(body, lookup, key, classInfo) {
+    let inputDiv = document.createElement('div');
+    inputDiv.setAttribute('class', 'col-lg-3 col-md-3 form-check');
+
+    //define label
+    let label = document.createElement('label');
+    label.setAttribute('class', 'form-check-label ' + classInfo + '-label');
+    label.setAttribute('for', key);
+    if (key === "B") {
+        label.innerHTML = lookup[key] + " " + "<img class=\"tagCheckboxImg\" src=\"https://www.seezeit.com/fileadmin/template/images/icons/speiseplan/stern.png\" />";
+    } else {
+        label.innerHTML = lookup[key] + " " + "<img class=\"tagCheckboxImg\" src=\"https://www.seezeit.com/fileadmin/template/images/icons/speiseplan/" + key + ".png\" />";
+    }
+
+    //define input
+    let input = document.createElement('input');
+    input.setAttribute('class', 'form-check-input ' + classInfo + '-checkbox');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('id', key);
+
+    //append checkbox and label to inputDiv
+    inputDiv.appendChild(input);
+    inputDiv.appendChild(label);
+
+    rowElement.appendChild(inputDiv);
+
+    if (rowElement.children.length == 4) {
+        body.appendChild(rowElement);
+        rowElement = document.createElement('div');
+        rowElement.setAttribute('class', 'row')
+    }
+}
---
+++ frontend/js/main.js
@@ -0,0 +1,193 @@
+let excludeSup = [];
+let excludeTags = [];
+
+function onChangeCheckbox() {
+    let url = "/api";
+    let data;
+    $("input[type='checkbox']").on("change", function () {
+        if (this.checked) {
+            if (this.className.split(" ").includes("sups-checkbox")) {
+                excludeSup.push(this.id);
+            } else if (this.className.split(" ").includes("tags-checkbox")) {
+                excludeTags.push(this.id);
+            }
+        } else {
+            let index;
+            if (this.className.split(" ").includes("sups-checkbox")) {
+                index = excludeSup.indexOf(this.id);
+                if (index > -1) {
+                    excludeSup.splice(index, 1);
+                }
+            } else if (this.className.split(" ").includes("tags-checkbox")) {
+                index = excludeTags.indexOf(this.id);
+                if (index > -1) {
+                    excludeTags.splice(index, 1);
+                }
+            }
+        }
+        let menuToBeFiltered = JSON.parse(JSON.stringify(menuAll));
+        showMenu(filterMenu(menuToBeFiltered, excludeSup, excludeTags), globalSelectedDate);
+    })
+}
+
+function showMenu(menu, day) {
+    let tableBody = document.getElementById("dishes");
+    document.getElementById("dishes").innerHTML = "";
+    //let dishes = menu[day].dishes;
+    let menuToFind = menu.find((toFind) => {
+        toFind = new Date(toFind.date);
+        toFind.setHours(0, 0, 0);
+        if (toFind.toDateString() === day.toDateString()) {
+            return true;
+        }
+        return false;
+    });
+    if (!menuToFind) {
+        console.log("couldn't find anything");
+        return;
+    }
+    let parser = new DOMParser()
+    menuToFind.dishes.forEach((dish) => {
+        let dishItemList = dish.Name.split("|");
+        for (let i = 0; i < dishItemList.length; i++) {
+            let dishListRegexMatch = dishItemList[i].match(/\(\d([a-zA-Z]|\d|,)*\)/g);
+            let name = dishItemList[i].replace(dishListRegexMatch, "").trim();
+            let supplements = dishListRegexMatch ? dishListRegexMatch.toString() : "";
+            if (supplements !== "") {
+                supplements = supplements.replace(/\(|\)/g, "");
+                let splittedSups = supplements.split(",");
+                for (let j = 0; j < splittedSups.length; j++) {
+                    let allergen = supplementTranslation.Allergene[splittedSups[j]];
+                    let additive = supplementTranslation.Zusatzstoffe[splittedSups[j]];
+                    let tag = supplementTranslation.Kategorien[splittedSups[j]];
+                    if (allergen) {
+                        splittedSups[j] = "<span data-toggle=\"tooltip\" title=\"" + allergen + "\" data-placement=\"top\" class=\"badge badge-green " + splittedSups[j] + "\">" + splittedSups[j] + "</span>";
+                    } else if (additive) {
+                        splittedSups[j] = "<span data-toggle=\"tooltip\" title=\"" + additive + "\" data-placement=\"top\" class=\"badge badge-red " + splittedSups[j] + "\">" + splittedSups[j] + "</span>";
+                    }
+                }
+                supplements = splittedSups.join(" ");
+                // console.log(splittedSups);
+            }
+
+            dishItemList[i] = name + " " + supplements + " <br/>";
+        }
+        let tags = dish.Tags;
+        let newElement = "<tr>" +
+            "<th scope=\"row\">" + dish.Category + "</th>" +
+            "<td>" + dishItemList.join("") + "</td>" +
+            "<td>" + dish.Pricing + "</td>" +
+            "<td>";
+        tags.forEach((tag) => {
+            if (tag === "B") {
+                tag = "stern";
+            }
+            newElement += "<img class=\"tagImg\" src=\"https://www.seezeit.com/fileadmin/template/images/icons/speiseplan/" + tag + ".png\" />"
+        })
+        newElement += "</td></tr>";
+        let el = parser.parseFromString(newElement, "text/xml");
+        tableBody.insertAdjacentHTML("beforeend", newElement);
+    })
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip({
+            boundary: 'window'
+        })
+    });
+}
+
+/**
+ * Removes all items with the exclude tags or supplements
+ * @param {*} menuItems String menu
+ * @param {*} excludeTags Array with tags as strings (Veg, Vegan, Sch, R, G, L, W, F, B)
+ * @param {*} excludeSup Array with supplements as strings.
+ */
+function filterMenu(menus, excludeSup, excludeTags) {
+    menus.forEach((menu) => {
+        let filtered = [];
+        menu.dishes.forEach((item) => {
+            let tags = item.Tags;
+            let supplements = getSupplements(item.Name);
+            if (!excludeSup) {
+                console.log("not");
+            }
+            if (!tags.find(m => excludeTags.includes(m)) && !supplements.some(m => excludeSup.includes(m))) {
+                filtered.push(item);
+            }
+        });
+        menu.dishes = filtered;
+    })
+    return menus;
+}
+
+/**
+ * Returns all supplements found in a string
+ * @param {*} string String with supplement list in the form of /\(\d.*\)/
+ */
+function getSupplements(string) {
+    let ingredients = string.split("|");
+    let supplements = [];
+    ingredients.forEach((ing) => {
+        let throwOut = false;
+        let res = ing.match(/\(\d([a-zA-Z]|\d|,)*\)/g);
+        if (res) {
+            res.forEach((match) => {
+                match = match.replace("(", "").replace(")", "");
+                match.split(",").forEach((elem) => {
+                    if (!supplements.includes(elem)) {
+                        supplements.push(elem);
+                    }
+                });
+            })
+        }
+    })
+    return supplements.sort();
+}
+
+function previous() {
+    let currentMenu = matchMenuDay(menuAll, globalSelectedDate);
+    if (currentMenu != -1) {
+        if (menuAll[currentMenu - 1]) {
+            globalSelectedDate = new Date(menuAll[currentMenu - 1].date);
+            menuDatePickr.setDate(globalSelectedDate);
+            //showMenu(menuAll, globalSelectedDate);
+            let menuToBeFiltered = JSON.parse(JSON.stringify(menuAll));
+            showMenu(filterMenu(menuToBeFiltered, excludeSup, excludeTags), globalSelectedDate);
+        }
+    }
+    if (globalSelectedDate.toDateString() === menuFirstDay.toDateString()) {
+        document.getElementById("button-previous").classList.add("disabled");
+    }
+    if (globalSelectedDate.toDateString() !== menuLastDay.toDateString()) {
+        document.getElementById("button-next").classList.remove("disabled");
+    }
+}
+
+function next() {
+    let currentMenu = matchMenuDay(menuAll, globalSelectedDate);
+    if (currentMenu != -1) {
+        if (menuAll[currentMenu + 1]) {
+            globalSelectedDate = new Date(menuAll[currentMenu + 1].date);
+            menuDatePickr.setDate(globalSelectedDate);
+            //showMenu(menuAll, globalSelectedDate);
+            let menuToBeFiltered = JSON.parse(JSON.stringify(menuAll));
+            showMenu(filterMenu(menuToBeFiltered, excludeSup, excludeTags), globalSelectedDate);
+        }
+    }
+    if (globalSelectedDate.toDateString() === menuLastDay.toDateString()) {
+        document.getElementById("button-next").classList.add("disabled");
+    }
+    if (globalSelectedDate.toDateString() !== menuFirstDay.toDateString()) {
+        document.getElementById("button-previous").classList.remove("disabled");
+    }
+}
+
+function matchMenuDay(menu, day) {
+    for (let i = 0; i < menu.length; i++) {
+        toFind = new Date(menu[i].date);
+        toFind.setHours(0, 0, 0);
+        if (toFind.toDateString() === day.toDateString()) {
+            return i;
+        }
+    }
+    return -1;
+}
