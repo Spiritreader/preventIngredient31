7986b2311f47d5d2508232cbc2bcc2a27b847977
Sam <earthstamper@gmail.com>
2019-04-19 15:17:30 +0200

    create folder structure for multi lang support

---
+++ frontend/lang/de.js
@@ -0,0 +1,58 @@
+{
+    supplements = {
+        "allergens": {
+            "25a": "Weizen",
+            "25b": "Roggen",
+            "25c": "Gerste",
+            "25d": "Dinkel",
+            "25e": "Hafer",
+            "25f": "Kamut",
+            "26": "Fisch",
+            "27": "Krusten- und Schalentiere",
+            "28": "Ei",
+            "29": "Erdnüsse",
+            "30": "Soja",
+            "31": "Milch & Laktose",
+            "32a": "Mandeln",
+            "32b": "Queenslandnüsse",
+            "32c": "Haselnüsse",
+            "32d": "Pecannüsse",
+            "32e": "Paranüsse",
+            "32f": "Macadamianüsse",
+            "32g": "Cashewnüsse",
+            "32h": "Walnüsse",
+            "32i": "Pistazien",
+            "33": "Sellerie",
+            "34": "Senf",
+            "35": "Sesam",
+            "36": "Schwefeloxide",
+            "37": "Lupine",
+            "38": "Weichtiere"
+        },
+        "additives": {
+            "1": "Farbstoffe",
+            "2": "Konservierungsstoffe",
+            "3": "Antioxidationsmittel",
+            "4": "Geschmacksverstärker",
+            "5": "geschwefelt",
+            "6": "geschwärzt",
+            "7": "gewachst",
+            "8": "Phosphat",
+            "9": "Süßungsmitteln",
+            "10": "enthält Phenylalaninquelle",
+            "11": "genetisch verändert",
+            "12": "enthält Gentech-Sojaöl"
+        },
+        "categories": {
+            "Veg": "Vegetarisch",
+            "Vegan": "Vegan",
+            "Sch": "Schwein",
+            "R": "Rind/Kalb",
+            "G": "Geflügel",
+            "L": "Lamm",
+            "W": "Wild",
+            "F": "Fisch",
+            "B": "Bessere Tierhaltung"
+        }
+    }
+}
